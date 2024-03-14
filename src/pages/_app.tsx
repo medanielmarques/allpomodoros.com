7d5fdcf3dd742cf83006cf69ed23ea37c998447e
Daniel Marques <daniel.brz2009@gmail.com>
2024-03-13 21:19:43 -0300

    Update dependencies and remove unused files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const FontOddval = localFont({ src: "../../public/fonts/oddval.woff" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${FontOddval.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
