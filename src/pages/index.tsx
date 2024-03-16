1482fc17e79e94b871a0d9ea7780dd4d91eb1c4a
Daniel Marques <daniel.brz2009@gmail.com>
2024-03-16 19:42:01 -0300

    Update imports and styles, add submit-app API endpoint and form handling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,11 +54,25 @@ export const getServerSideProps = async () => {
 }
 
 export default function Home({ apps = [] }) {
+  async function handleSubmitApp(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    await fetch("/api/submit-app", {
+      method: "POST",
+      body: formData,
+    })
+
+    console.log(formData.get("email"))
+    console.log(formData.get("twitter"))
+    console.log(formData.get("projectUrl "))
+  }
+
   return (
-    <main className="relative flex min-h-screen flex-col items-center bg-black p-4 text-white bg-dot-white/[0.2]">
-      <div className="max-w-5xl">
+    <main className="relative flex min-h-screen flex-col items-center gap-12 bg-black text-white bg-dot-white/[0.2]">
+      <div className="max-w-5xl p-4">
         <div className="mt-4">
-          <p>Pomodoro Plaza</p>
+          <p>All Pomodoros</p>
         </div>
 
         <div className="mx-auto w-1/2">
@@ -71,42 +85,63 @@ export default function Home({ apps = [] }) {
 
         <footer className="text-center">
           <Link href="https://x.com/medanielmarques" target="_blank">
-            <button className="rounded-lg bg-indigo-700 p-4 hover:bg-indigo-600">
+            <button className="rounded-lg bg-indigo-700 px-4 py-3 hover:bg-indigo-600">
               Made by → @medanielmarques
             </button>
           </Link>
         </footer>
       </div>
 
-      <div className="container flex items-center justify-center bg-neutral-900 p-12">
-        <form action="" method="post">
-          <div className="flex flex-col gap-4">
-            <div className="flex gap-4">
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="email">
-                  <span className="text-red-600">*</span> Your email
-                </Label>
-                <Input type="email" id="email" placeholder="john@doe.io" />
-              </div>
-
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="email">Your twitter</Label>
-                <Input type="email" id="email" placeholder="@johndoe" />
-              </div>
-            </div>
-
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Label htmlFor="email">
-                <span className="text-red-600">*</span> Project URL
-              </Label>
-              <Input
-                type="email"
-                id="email"
-                placeholder="Enter the public url of your project"
-              />
-            </div>
+      <div className="container flex justify-center bg-neutral-950 p-12">
+        <div className="max-w-1/3 flex flex-col gap-6">
+          <div className="m-auto flex w-3/4 flex-col justify-center gap-4 text-center">
+            <h2 className="text-2xl font-bold md:text-3xl">
+              Submit your Pomodoro app
+            </h2>
+            <p>Get more visibility for your Pomodoro Timer App.</p>
           </div>
-        </form>
+
+          <form onSubmit={handleSubmitApp}>
+            <div className="flex flex-col items-center gap-4">
+              <div className="flex w-full flex-col gap-4 md:flex-row">
+                <div className="items-center gap-3">
+                  <Label htmlFor="email">
+                    <span className="text-red-600">*</span> Your email
+                  </Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="john@doe.io"
+                  />
+                </div>
+
+                <div className="items-center gap-1.5">
+                  <Label htmlFor="twitter">Your twitter</Label>
+                  <Input type="text" id="twitter" placeholder="@johndoe" />
+                </div>
+              </div>
+
+              <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor="project-url">
+                  <span className="text-red-600">*</span> Project URL
+                </Label>
+                <Input
+                  type="text"
+                  id="project-url"
+                  placeholder="Enter the public url of your project"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="mt-3 w-full rounded-lg bg-indigo-700 px-4 py-3 hover:bg-indigo-600 md:mt-6 md:w-auto"
+              >
+                Submit your project →
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   )
